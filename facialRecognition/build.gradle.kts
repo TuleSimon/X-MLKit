97fbac9487157cac1f00685f80597d6dc4babe7f
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-01-28 15:55:14 +0100

    migrated to kotlin Dsl from groovy16

--- facialRecognition/build.gradle.kts
+++ facialRecognition/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.simon.x_mlkit.facialrecognition"
-    compileSdk = 33
+    compileSdk = Versions.COMPILE_SDK_VERSION
 
     defaultConfig {
-        minSdk = 21
-        targetSdk = 33
+        minSdk = Versions.MIN_SDK_VERSION
+        targetSdk = Versions.MAX_SDK_VERSION
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -35,10 +35,4 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.0")
-    implementation("com.google.android.material:material:1.7.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
