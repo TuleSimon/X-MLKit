ed0b088b9e832f0f1849e8faaafad4ac1912f944
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-01-28 15:20:39 +0100

    migrated to kotlin Dsl from groovy5

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,21 +1,23 @@
 package com.simon.x_mlkit
 
+
 object Dependencies {
 
-
-    implementation()
-    implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha04")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
-    debugImplementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:${rootProject.extra["compose_version"]}")
   const val ANDROID_CORE_KTS ="androidx.core:core-ktx:1.9.0"
   const val ANDROID_LIFECYCLE_RUNTIME_KTX ="androidx.lifecycle:lifecycle-runtime-ktx:2.5.1"
   const val ANDROIDX_ACTIVITY_COMPOSE ="androidx.activity:activity-compose:1.6.1"
   const val ANDROIDX_COMPOSE_UI ="androidx.compose.ui:ui:${Versions.COMPOSE_VERSION}"
-  const val ANDROIDX ="androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_version"]}"
+  const val ANDROIDX_COMPOSE_UI_TOOLING_PREVIEW ="androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE_VERSION}"
+  const val ANDROIDX_COMPOSE_MATERIAL3 ="androidx.compose.material3:material3:1.1.0-alpha04"
+
+
+  const val JUNIT ="junit:junit:4.13.2"
+  const val ANDROIDX_JUNIT_TEST ="androidx.test.ext:junit:1.1.5"
+  const val ESPRESSO_CORE ="androidx.test.espresso:espresso-core:3.5.1"
+  const val ANDROIDX_COMPOSE_UI_TEST_JUNIT4 ="androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE_VERSION}"
+  const val ANDROIDX_COMPOSE_UI_TOOLING ="androidx.compose.ui:ui-tooling:${Versions.COMPOSE_VERSION}"
+  const val ANDROIDX_COMPOSE_UI_MANIFEST_TEST ="androidx.compose.ui:ui-test-manifest:${Versions.COMPOSE_VERSION}"
 
 }
+
+fun DependencyHandler
