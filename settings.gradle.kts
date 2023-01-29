be1f69a9f2d43529f7a66bc7d2e8e9eb819ada5b
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-01-29 19:54:15 +0100

    integrating huwai sdk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://developer.huawei.com/repo/")
     }
 }
 dependencyResolutionManagement {
@@ -15,6 +16,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://developer.huawei.com/repo/")
     }
 }
 rootProject.name = "X-MLKIT"
