ff6de1c5549dbe5842ca53566d9dadf7ba5b1916
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-01-31 12:34:27 +0100

    added camerax module8

--- cameraXCompose/src/main/java/com/simon/cameraxcompose/CameraX.kt
+++ cameraXCompose/src/main/java/com/simon/cameraxcompose/CameraX.kt
@@ -1,11 +1,20 @@
 package com.simon.cameraxcompose
 
-internal fun cameraPreview(){
+import android.view.ViewGroup
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+internal fun cameraPreview(
+    modifier: Modifier
+){
     AndroidView(modifier = modifier, factory = { context ->
         PreviewView(context).apply {
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
             )
             }
-        }
+        })
 }
