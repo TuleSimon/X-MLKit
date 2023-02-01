b037a9ea230be1054d91364b9303ec155a57a92e
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-02-01 20:32:50 +0100

    added camerax module24

--- cameraXCompose/src/main/java/com/simon/cameraxcompose/CameraX.kt
+++ cameraXCompose/src/main/java/com/simon/cameraxcompose/CameraX.kt
@@ -13,29 +13,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @Composable
- fun CameraPreview(modifier: Modifier,
-lifecycleOwner: LifecycleOwner) {
-  AndroidView(
-      modifier = modifier,
-      factory = { context ->
-        PreviewView(context).apply {
-          layoutParams =
-              ViewGroup.LayoutParams(
-                  ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+fun CameraPreview(modifier: Modifier,
+                  lifecycleOwner: LifecycleOwner) {
+    val cameraExecutor: ExecutorService  = Executors.newSingleThreadExecutor()
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            PreviewView(context).apply {
+                layoutParams =
+                    ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            }
+        },
+        update = { previewView ->
+            startCamera(previewView,lifecycleOwner)
         }
-      },
-      update = { previewView ->
-
-      }
-  )
+    )
 }
 
 
 internal fun startCamera(previewView: PreviewView,
-lifecycleOwner: LifecycleOwner) {
-    val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+                         lifecycleOwner: LifecycleOwner) {
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(previewView.context)
 
     cameraProviderFuture.addListener({
         // Used to bind the lifecycle of cameras to the lifecycle owner
@@ -57,7 +60,7 @@ lifecycleOwner: LifecycleOwner) {
 
             // Bind use cases to camera
             cameraProvider.bindToLifecycle(
-                 lifecycleOwner, cameraSelector, preview)
+                lifecycleOwner, cameraSelector, preview)
 
         } catch(exc: Exception) {
             Log.e("XCAMERA", "Use case binding failed", exc)
@@ -67,13 +70,13 @@ lifecycleOwner: LifecycleOwner) {
 }
 
 
-    private val REQUIRED_PERMISSIONS =
-        mutableListOf (
-            Manifest.permission.CAMERA,
-            Manifest.permission.RECORD_AUDIO
-        ).apply {
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            }
-        }.toTypedArray()
+private val REQUIRED_PERMISSIONS =
+    mutableListOf (
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO
+    ).apply {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+    }.toTypedArray()
 
