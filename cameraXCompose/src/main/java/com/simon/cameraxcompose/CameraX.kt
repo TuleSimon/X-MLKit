713c5e49e26d6a92f1346bc48da6b4940a3d209d
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-02-02 19:43:26 +0100

    added camerax module29

--- cameraXCompose/src/main/java/com/simon/cameraxcompose/CameraX.kt
+++ cameraXCompose/src/main/java/com/simon/cameraxcompose/CameraX.kt
@@ -5,6 +5,7 @@ import android.os.Build
 import android.util.Log
 import android.view.ViewGroup
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
@@ -67,6 +68,49 @@ internal fun startCamera(previewView: PreviewView,
         }
 
     }, ContextCompat.getMainExecutor(previewView.context))
+}
+
+
+private fun takePhoto(previewView: PreviewView) {
+    // Get a stable reference of the modifiable image capture use case
+    val imageCapture =ImageCapture.Builder(). imageCapture ?: return
+
+    // Create time stamped name and MediaStore entry.
+    val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+        .format(System.currentTimeMillis())
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+        }
+    }
+
+    // Create output options object which contains file + metadata
+    val outputOptions = ImageCapture.OutputFileOptions
+        .Builder(contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues)
+        .build()
+
+    // Set up image capture listener, which is triggered after photo has
+    // been taken
+    imageCapture.takePicture(
+        outputOptions,
+        ContextCompat.getMainExecutor(this),
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exc: ImageCaptureException) {
+                Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+            }
+
+            override fun
+                    onImageSaved(output: ImageCapture.OutputFileResults){
+                val msg = "Photo capture succeeded: ${output.savedUri}"
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, msg)
+            }
+        }
+    )
 }
 
 
