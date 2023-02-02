879fe2c4b42bc872ec0e7eccb34691a08686426e
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-02-02 20:19:36 +0100

    added camerax module44

--- app/src/main/java/com/simon/x_mlkit/MainActivity.kt
+++ app/src/main/java/com/simon/x_mlkit/MainActivity.kt
@@ -7,15 +7,16 @@ import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.FileProvider
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -23,74 +24,80 @@ import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.simon.cameraxcompose.CameraPreview
+import com.simon.cameraxcompose.states.rememberCameraXState
 import com.simon.x_mlkit.ui.theme.XMLKITTheme
 import java.io.File
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalPermissionsApi::class)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            val succes = remember { mutableStateOf(false) }
-            val imageUri = remember { mutableStateOf<Uri?>(null) }
+  @OptIn(ExperimentalPermissionsApi::class)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      XMLKITTheme {
+        val succes = remember { mutableStateOf(false) }
+        val imageUri = remember { mutableStateOf<Uri?>(null) }
 
-            XMLKITTheme {
-                LaunchedEffect(key1 = succes.value) {
-                    if (succes.value) {
-                        imageUri.value?.let {
-                            val bitmap = MediaStore.Images.Media.getBitmap(this@MainActivity.contentResolver, it)
-                            bitmap?.let {}
-                        }
-                    }
-                }
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(20.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier.fillMaxSize()) {
-
-                        val state = requestCameraPermission()
-
-                        LaunchedEffect(true) {
-                            state.launchPermissionRequest()
-                        }
-
-                        if (state.status.isGranted) {
-                            CameraPreview(modifier = Modifier.fillMaxSize(),this@MainActivity)
-                        }
-                    }
-                }
+        LaunchedEffect(key1 = succes.value) {
+          if (succes.value) {
+            imageUri.value?.let {
+              val bitmap = MediaStore.Images.Media.getBitmap(this@MainActivity.contentResolver, it)
+              bitmap?.let {}
             }
+          }
         }
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          Column(
+              verticalArrangement = Arrangement.spacedBy(20.dp),
+              horizontalAlignment = Alignment.CenterHorizontally,
+              modifier = Modifier.fillMaxSize()) {
+                val state = requestCameraPermission()
+
+                LaunchedEffect(true) { state.launchPermissionRequest() }
+
+                if (state.status.isGranted) {
+                  val cameraState = rememberCameraXState()
+                  CameraPreview(modifier = Modifier.fillMaxSize(), cameraState)
+                  Button(
+                      modifier = Modifier.padding(15.dp).fillMaxWidth(),
+                      contentPadding = PaddingValues(20.dp),
+                      onClick = { /*TODO*/}) {
+                        Text(text = "CAPTURE PHOTO")
+                        //
+                      }
+                }
+              }
+        }
+      }
     }
+  }
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun requestCameraPermission(): PermissionState {
-    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+  val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
 
-    return cameraPermissionState
+  return cameraPermissionState
 }
 
 class ComposeFileProvider : FileProvider(R.xml.filepaths) {
-    companion object {
-        fun getImageUri(context: Context): Uri {
-            val directory = File(context.cacheDir, "images")
-            directory.mkdirs()
-            val file =
-                File.createTempFile(
-                    "selected_image_",
-                    ".jpg",
-                    directory,
-                )
-            val authority = context.packageName + ".fileprovider"
-            return getUriForFile(
-                context,
-                authority,
-                file,
-            )
-        }
+  companion object {
+    fun getImageUri(context: Context): Uri {
+      val directory = File(context.cacheDir, "images")
+      directory.mkdirs()
+      val file =
+          File.createTempFile(
+              "selected_image_",
+              ".jpg",
+              directory,
+          )
+      val authority = context.packageName + ".fileprovider"
+      return getUriForFile(
+          context,
+          authority,
+          file,
+      )
     }
+  }
 }
